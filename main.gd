064ba661cd4f04075590a135f7a96621567683c7
aiworkdmitr-ctrl <aiworkdmitr@gmail.com>
2026-04-18 14:27:00 +0300

    Create main.gd as the main game controller script

---
+++ main.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+# The main game controller script for the visual novel.
+class_name Main
+
+# This function is called when the node enters the scene tree for the first time.
+func _ready():
+    # Start the game logic here
+    pass
+
+# Function to manage game states
+func change_state(new_state):
+    # Logic for changing game states
+    pass
